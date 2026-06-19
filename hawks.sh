6e8d418ebfcd040c9c430f6f55a028786e10127e
ticktechman <geek.wystan@gmail.com>
2026-06-19 16:29:13 +0800

    change dts chosen stdout to hvc0

--- hawks.sh
+++ hawks.sh
@@ -8,6 +8,7 @@
 ##
 ###############################################################################
 
+dtc -I dts -O dtb -o platform/hawks/hawks.dtb platform/hawks/hawks.dts
 make CROSS_COMPILE=riscv64-linux-gnu- PLATFORM=hawks
 riscv64-linux-gnu-objcopy -O verilog --verilog-data-width=1 ./build/platform/hawks/firmware/fw_payload.elf sbi.hex
 
